5b0fbd25f506bd4f37b1b8c33f84a487319569e7
Sifat Bhatia <sifatbhatia@yandex.com>
2025-02-21 18:43:44 -0800

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    inject( <App />)
-
+    inject()
+    <App />
   </React.StrictMode>
 )
